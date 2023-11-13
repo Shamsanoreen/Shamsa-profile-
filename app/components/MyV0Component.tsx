b4d68b80ee7b74eac3195dceb31e995e15d9d088
Shamsa Noreen <150453213+Shamsanoreen@users.noreply.github.com>
2023-11-13 15:49:18 +0000

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,62 +1,74 @@
-// you can overwrite this entire file with your v0 Component.
-// just copy and paste the "React" output over the entire file.
-
-import Link from "next/link";
-import React from "react";
-import {V0Logo} from "./symbols";
-
-function TestComponent() {
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/8fwGoOO
+ */
+export default function Component() {
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      <h3
-        style={{
-          maxWidth: "11em",
-        }}
-      >
-        This would be a pretty good place for a{" "}
-        <Link href="https://v0.dev/" target="_blank" rel="noopener noreferrer">
-          v0 component
-        </Link>
-        , wouldn't it?
-      </h3>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-          width: "100%",
-        }}
-      >
-        <p
-          style={{
-            maxWidth: "20em",
-          }}
-        >
-          Go make one, then paste it into{" "}
-          <code
-            style={{
-              fontFamily: "var(--font-geist-mono)",
-              fontWeight: 500,
-              fontSize: "0.95em",
-              fontFeatureSettings: "'ss09'",
-            }}
-          >
-            app/components/MyV0Component.tsx
-          </code>
-        </p>
-
-        <V0Logo />
-      </div>
+    <div className="grid grid-cols-8 gap-2 p-4 bg-white dark:bg-gray-800 rounded-md shadow-lg">
+      <div className="flex items-center justify-center bg-black text-white">♜</div>
+      <div className="flex items-center justify-center bg-white text-black">♞</div>
+      <div className="flex items-center justify-center bg-black text-white">♝</div>
+      <div className="flex items-center justify-center bg-white text-black">♛</div>
+      <div className="flex items-center justify-center bg-black text-white">♚</div>
+      <div className="flex items-center justify-center bg-white text-black">♝</div>
+      <div className="flex items-center justify-center bg-black text-white">♞</div>
+      <div className="flex items-center justify-center bg-white text-black">♜</div>
+      <div className="flex items-center justify-center bg-white text-black">♟</div>
+      <div className="flex items-center justify-center bg-black text-white">♟</div>
+      <div className="flex items-center justify-center bg-white text-black">♟</div>
+      <div className="flex items-center justify-center bg-black text-white">♟</div>
+      <div className="flex items-center justify-center bg-white text-black">♟</div>
+      <div className="flex items-center justify-center bg-black text-white">♟</div>
+      <div className="flex items-center justify-center bg-white text-black">♟</div>
+      <div className="flex items-center justify-center bg-black text-white">♟</div>
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-white" />
+      <div className="flex items-center justify-center bg-black" />
+      <div className="flex items-center justify-center bg-black text-white">♙</div>
+      <div className="flex items-center justify-center bg-white text-black">♙</div>
+      <div className="flex items-center justify-center bg-black text-white">♙</div>
+      <div className="flex items-center justify-center bg-white text-black">♙</div>
+      <div className="flex items-center justify-center bg-black text-white">♙</div>
+      <div className="flex items-center justify-center bg-white text-black">♙</div>
+      <div className="flex items-center justify-center bg-black text-white">♙</div>
+      <div className="flex items-center justify-center bg-white text-black">♙</div>
+      <div className="flex items-center justify-center bg-white text-black">♖</div>
+      <div className="flex items-center justify-center bg-black text-white">♘</div>
+      <div className="flex items-center justify-center bg-white text-black">♗</div>
+      <div className="flex items-center justify-center bg-black text-white">♕</div>
+      <div className="flex items-center justify-center bg-white text-black">♔</div>
+      <div className="flex items-center justify-center bg-black text-white">♗</div>
+      <div className="flex items-center justify-center bg-white text-black">♘</div>
+      <div className="flex items-center justify-center bg-black text-white">♖</div>
     </div>
-  );
+  )
 }
-
-export default TestComponent;
